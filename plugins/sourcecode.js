c72726a7c8608096ae1e33be7ddec74d371e86b1
HanzBotzz <109465690+HanzBotzz@users.noreply.github.com>
2022-07-21 08:52:13 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,10 +1,24 @@
+/*
+ * ngelarang lu biar g diubah kyknya emg susah ya anjg
+ * jadi kalau lu g bsa ngehargain orang
+ * setidaknya buat sc ori by gw, trs buat sc lu hasil copas tuh
+ * better bgtu dripada nma gw dihapus smua
+ * najis 👎🗿
+*/
+
 let handler = async (m, { conn }) => {
-     conn.reply(m.chat, `Bot ini menggunakan script github\n\nhttps://github.com/HanzBotzz/Ryan-Md\n\npliss follow pluss kasih ✨ ya pak!`, m)
+let ye = `@${m.sender.split`@`[0]}`
+let esce = `
+Hai ${ye} Lagi Nyari Sc Ya? 
+
+Sc private klo mau cht owner Dibawah :
+wa.me/6285779150501
+
+_*Jangan Lupa Di Subscribe*_`
+conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
-handler.help = ['sourcecode']
+handler.help = ['sc', 'sourcecode']
 handler.tags = ['info']
-handler.command = /^(sc(ript(bot)?)?|sourcecode)$/i
+handler.command = /^(sc|sourcecode)$/i
 
 module.exports = handler
-
-
